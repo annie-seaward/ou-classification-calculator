10e463043c472aa66bf80e12fb70293ab25ab8c3
annie-seaward <annie.seaward@gmail.com>
2022-09-02 12:27:00 +0100

    Adding first classification check

--- app.js
+++ app.js
@@ -48,6 +48,20 @@ function checkCredits (arr) {
     return ({totalPoints, correctCredits});
 }
 
+function firstCheck (total) {
+    if (total <= 630) {
+        return 1
+    } else if (total <= 900) {
+        return 2
+    } else if (total <= 1170) {
+        return 3
+    } else if (total <= 1440) {
+        return 4
+    } else {
+        return 5
+    }
+}
+
 let level2 = checkCredits(courses.level2);
 
 let level3 = checkCredits(courses.level3);
@@ -56,6 +70,7 @@ level3.totalPoints = level3.totalPoints * 2;
 
 if (level2.correctCredits && level3.correctCredits) {
     console.log("Total Credits " + (level2.totalPoints + level3.totalPoints));
+    console.log("Classification " + firstCheck(level2.totalPoints + level3.totalPoints));
 } else {
     console.log("Incorrect number of credits");
 }
